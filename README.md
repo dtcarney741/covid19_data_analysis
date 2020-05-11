192ca49ad12b4814870a0a4df81e9eaa659e1083
Aiden Carney <ajcarney314@gmail.com>
2020-05-11 14:56:40 -0500

    README
    *Updated README and added liscense
    *added requirements.txt file to install python library requirements
    *Made data grabber more portable by using os library to make paths

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # covid19_data_analysis
-script for parsing and plotting data from Johns Hopkins COVID-19 repository
+script for parsing and plotting data from Johns Hopkins COVID-19 data repository
+
+https://github.com/CSSEGISandData/COVID-19
+
+This script allows users to plot many different graphs relating to COVID-19 data:
+* Daily Rate of Confirmed Cases to People Tested vs. Date
+* Daily Number of People Tested vs. Date
+* Daily Cases per 1mil population vs. Date
+* Confirmed Cases vs. Date
+
+This project was built using python3 and curl (to automatically grab data)
+
+## Install
+
+Clone repository and run ```pip install -r requirements.txt```. This will install all python dependencies
+
+Other dependencies include ```curl```
+
+## Usage
+
+Run ```python covid19_data_analysis.py``` or ```python3 covid19_data_analysis.py``` depending on os
+
+## Sample Output 
+<p align="center">
+  <img src="img.jpeg" width="350">
+</p>
+
+## License
+[gpl-3.0](https://opensource.org/licenses/lgpl-3.0.html)
+
+## Project Status
+In development with daily to weekly commits
+
+## Features coming soon
+* Better error handling
+* Easier UI
+* Increased Portability
+* Portability Testing
+* Derivative Graphs
+* Example Output
--- data_grabber.py
+++ data_grabber.py
@@ -6,34 +6,34 @@ import os
 
 def retrieve_data():
     """
-    uses git shell command to retrieve the data
+    uses curl shell command to retrieve the data
     returns output directory
     
     returns path of folder that data in contained in
     ex. ./"date"/"folder name"
     """
-    if os.path.isfile("./data.zip"):  # remove old zipped data
-        os.remove("./data.zip")
+    if os.path.isfile(os.path.join(os.path.dirname("."), "data.zip")):  # remove old zipped data
+        os.remove(os.path.join(os.path.dirname("."), "data.zip"))
         
     subprocess.Popen("curl -L https://api.github.com/repos/CSSEGISandData/COVID-19/zipball/master --output data.zip", shell=True, stdout=subprocess.PIPE).wait()
     
     
     #make directories for data to be held
-    if not os.path.isdir("./data"):
-        os.mkdir("./data")
+    if not os.path.isdir(os.path.join(os.path.dirname("."), "data")):
+        os.mkdir(os.path.join(os.path.dirname("."), "data"))
     
-    output_folder = "./data/" + str(date.today())
+    output_folder = os.path.join(os.path.dirname("."), "data", str(date.today()))
     if os.path.isdir(output_folder):  # force remake of data folder
         shutil.rmtree(output_folder)
     os.mkdir(output_folder)
     
-    with zipfile.ZipFile("./data.zip", 'r') as zip_ref:
+    with zipfile.ZipFile(os.path.join(os.path.dirname("."), "data.zip"), 'r') as zip_ref:
         zip_ref.extractall(output_folder)
 
 
-    os.remove("./data.zip")  # cleanup
+    os.remove(os.path.join(os.path.dirname("."), "data.zip"))  # cleanup
     
-    data_folder = output_folder + "/" + str(os.listdir(output_folder)[0])
+    data_folder = os.path.join(output_folder, str(os.listdir(output_folder)[0]))
     return data_folder
 
 
