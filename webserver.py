57e998e9dfc3d8f200b12fac30fa15e63b2f61a0
Aiden Carney <ajcarney314@gmail.com>
2020-07-10 17:05:29 -0500

    Data Tables
    *Added 3 new modes with functionality other than just line charts. - Parsed Data time series, which has any amount of areas in a table with each row being a date with one type of data, Parsed Data daily reports, which has a table for each area with each table having all types of data, and File Data which allows user to download the data from the github repo

--- webserver.py
+++ webserver.py
@@ -6,10 +6,14 @@ Created on Mon Jun 15 22:06:37 2020
 @author: aiden
 """
 import copy
+import datetime
 import numpy as np
 import streamlit as st
+import os
+import pandas as pd
 
 import covid19_data
+import data_grabber
 import plot_handler
 
 
@@ -211,6 +215,51 @@ def get_label(node):
     return new_name.strip()
 
 
+def get_regions(root_node):
+    # find what areas to graph
+    region_default = ["US"]
+    if st.sidebar.radio("Add all Regions", ['Yes', 'No'], 1) == "Yes":
+        region_default = [i.node_name for i in root_node.get_children()]
+    regions = st.sidebar.multiselect("Select Regions", sorted([i.node_name for i in root_node.get_children()]), default=region_default)
+    regions_nodes = [root_node.get_child_node(name) for name in regions]
+    plotted_areas = []
+    for region in regions_nodes:
+        if region.get_children():
+            state_province_default = []
+            if st.sidebar.radio("Add all States/Provinces in " + region.node_name, ['Yes', 'No'], 1) == "Yes":
+                state_province_default = [i.node_name for i in region.get_children()]
+            sub_regions = st.sidebar.multiselect(
+                    ("State/Province in " + region.node_name), 
+                    sorted([i.node_name for i in region.get_children()]),
+                    default=state_province_default
+                )
+            if st.sidebar.checkbox("Add Data for " + region.node_name, value=True):
+                plotted_areas.append(region)
+        
+        
+            sub_regions_nodes = [region.get_child_node(name) for name in sorted(sub_regions)]
+            for sub_region in sub_regions_nodes:
+                if sub_region.get_children():
+                    county_default = []
+                    if st.sidebar.radio("Add all Counties in " + sub_region.node_name, ['Yes', 'No'], 1) == "Yes":
+                        county_default = [i.node_name for i in region.get_children()]
+                    counties = st.sidebar.multiselect(
+                        ("Counties in " + sub_region.node_name), 
+                        sorted([i.node_name for i in sub_region.get_children()]),
+                        default=county_default
+                    )
+                    plotted_areas.extend([sub_region.get_child_node(i) for i in counties])
+                    
+                    if st.sidebar.checkbox("Add Data for " + sub_region.node_name, value=True):
+                        plotted_areas.append(sub_region)
+                else:
+                    plotted_areas.append(sub_region)
+        else:
+            plotted_areas.append(region)    
+
+    return plotted_areas
+
+
 # # get the data to parse
 # file = "./data/2020-05-11/CSSEGISandData-COVID-19-5184bec/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv"
 # folder = "./data/2020-05-11/CSSEGISandData-COVID-19-5184bec/csse_covid_19_data/csse_covid_19_daily_reports_us"
@@ -262,7 +311,9 @@ mode = st.sidebar.selectbox(
     "Mode",
     [
         "Line Chart",
-        "Data"
+        "Parsed Data - Time Series",
+        "Parsed Data - Daily Reports",
+        "File Data"
     ]
 )
 # select the type of graph
@@ -277,115 +328,95 @@ data_options = {
     "Recovered Cases":"recovered_cases_time_series_data",
     "Recovery Rate":"get_recovery_rate",
     "Confirmed Cases to People Tested Ratio":"get_ratio_confirmed_cases_to_people_tested"
-    }
+}
 
-graph_types1 = st.sidebar.multiselect("Axis 1", sorted(data_options.keys()), default=["Confirmed Cases"])
-graph_types2 = st.sidebar.multiselect("Axis 2", sorted(data_options.keys()), default=["Confirmed Cases"])
-
-
-
-# find what areas to graph
-regions = st.sidebar.multiselect("Select Regions", sorted([i.node_name for i in world_node.get_children()]), default=["US"])
-regions_nodes = [world_node.get_child_node(name) for name in regions]
-plotted_areas = []
-for region in regions_nodes:
-    if region.get_children():
-        sub_regions = st.sidebar.multiselect(
-                ("State/Province in " + region.node_name), 
-                sorted([i.node_name for i in region.get_children()])
-            )
-        if st.sidebar.checkbox("Plot Data for " + region.node_name, value=True):
-            plotted_areas.append(region)
-    
-    
-        sub_regions_nodes = [region.get_child_node(name) for name in sorted(sub_regions)]
-        for sub_region in sub_regions_nodes:
-            if sub_region.get_children():
-                counties = st.sidebar.multiselect(
-                    ("Counties in " + sub_region.node_name), 
-                    sorted([i.node_name for i in sub_region.get_children()])
-                )
-                plotted_areas.extend([sub_region.get_child_node(i) for i in counties])
-                
-                if st.sidebar.checkbox("Plot Data for " + sub_region.node_name, value=True):
-                    plotted_areas.append(sub_region)
-            else:
-                plotted_areas.append(sub_region)
-    else:
-        plotted_areas.append(region)
-        
-        
-# add original datasets to the plot handler     
-dates = covid_data.time_series_dates 
-int_to_dates_lookup, dates_to_int_lookup = covid_data.create_lookup_tables(min(dates), max(dates))
-x = []
-
-# create look up tables
-for _ in range(len(plotted_areas)):
-    x.append([dates_to_int_lookup.get(date.strftime("%m-%d-%Y")) for date in dates])
-
-get_graph_session_info().set_dates_data(x)
-get_graph_session_info().set_lookup_table(int_to_dates_lookup)
-get_graph_session_info().set_plotted_regions(plotted_areas)
-   
-add_axis_title_to_legend = False
-
-plot_handlers_axis1 = []
-plot_handlers_axis2 = []
-
-y_axis1_titles = []
-y_axis2_titles = []
-x_axis_title = "Date"
-
-for i, graph in enumerate(graph_types1):
-    # handler = create_data_handler(int_to_dates_lookup, plotted_areas, x, graph, data_options.get(graph))
-    handler = get_graph_session_info().get_data_handler(graph, data_options.get(graph))
-    plot_handlers_axis1.append(handler)
-    y_axis1_titles.append(graph)
-    if i > 0:
-        add_axis_title_to_legend = True
-        
-for i, graph in enumerate(graph_types2):
-    # handler = create_data_handler(int_to_dates_lookup, plotted_areas, x, graph, data_options.get(graph))
-    handler = get_graph_session_info().get_data_handler(graph, data_options.get(graph))
-    plot_handlers_axis2.append(handler)
-    y_axis2_titles.append(graph)
-    if i > 0:
-        add_axis_title_to_legend = True
-
-plots_data = [plot_handlers_axis1, plot_handlers_axis2]
-y_axis_titles = [y_axis1_titles, y_axis2_titles]
-
-if len(plots_data) > 1:
-    add_axis_title_to_legend = True
-    
-
-    
-    
-if st.button("Refresh"):
+if st.button("Clear Cache"):
     st.caching.clear_cache()
 
+# get x axis data
+dates = covid_data.time_series_dates 
+int_to_dates_lookup, dates_to_int_lookup = covid_data.create_lookup_tables(min(dates), max(dates))
+    
+    
+if mode == "Line Chart":
+    graph_types1 = st.sidebar.multiselect("Axis 1", sorted(data_options.keys()), default=["Confirmed Cases"])
+    graph_types2 = st.sidebar.multiselect("Axis 2", sorted(data_options.keys()))
+    
+    plotted_areas = get_regions(world_node)
 
-# get configuration for each graph
-areas = []
-areas.append(st.empty())
-
-
-smooth_areas = []  # area for smoothing options
-for j in range(6):
-    smooth_areas.append(st.empty())
-data = get_configuration(0, smooth_areas, get_smoothing_options_session_info().get_settings(0)) 
-get_smoothing_options_session_info().set_settings(0, data)
-
-for dataset in plots_data:
-    for data_entry in dataset:
-        # configuration for first plot
-        data_entry.update_configuration(0, data)
-        data_entry.update_plot_data(0)
-
-
-user_interaction_areas = []
-for i in range(get_graph_session_info().num_derivatives):
+    # add original datasets to the plot handler     
+    x = []
+    
+    # create look up tables
+    for _ in range(len(plotted_areas)):
+        x.append([dates_to_int_lookup.get(date.strftime("%m-%d-%Y")) for date in dates])
+    
+    get_graph_session_info().set_dates_data(x)
+    get_graph_session_info().set_lookup_table(int_to_dates_lookup)
+    get_graph_session_info().set_plotted_regions(plotted_areas)
+       
+    add_axis_title_to_legend = False
+    
+    plot_handlers_axis1 = []
+    plot_handlers_axis2 = []
+    
+    y_axis1_titles = []
+    y_axis2_titles = []
+    x_axis_title = "Date"
+    
+    for i, graph in enumerate(graph_types1):
+        handler = get_graph_session_info().get_data_handler(graph, data_options.get(graph))
+        plot_handlers_axis1.append(handler)
+        y_axis1_titles.append(graph)
+        if i > 0:
+            add_axis_title_to_legend = True
+            
+    for i, graph in enumerate(graph_types2):
+        handler = get_graph_session_info().get_data_handler(graph, data_options.get(graph))
+        plot_handlers_axis2.append(handler)
+        y_axis2_titles.append(graph)
+        if i > 0:
+            add_axis_title_to_legend = True
+    
+    plots_data = [plot_handlers_axis1, plot_handlers_axis2]
+    y_axis_titles = [y_axis1_titles, y_axis2_titles]
+    
+    if len(plots_data) > 1:
+        add_axis_title_to_legend = True
+            
+    # get configuration for each graph
+    areas = []
+    areas.append(st.empty())
+    
+    smooth_areas = []  # area for smoothing options
+    for j in range(6):
+        smooth_areas.append(st.empty())
+    data = get_configuration(0, smooth_areas, get_smoothing_options_session_info().get_settings(0)) 
+    get_smoothing_options_session_info().set_settings(0, data)
+    
+    for dataset in plots_data:
+        for data_entry in dataset:
+            # configuration for first plot
+            data_entry.update_configuration(0, data)
+            data_entry.update_plot_data(0)
+    
+    
+    user_interaction_areas = []
+    for i in range(get_graph_session_info().num_derivatives):
+        partition = []
+        
+        partition.append(st.empty())  # area for derivative graph
+        
+        partition.append(st.empty())  # area for removing derivative
+        
+        smooth_areas = []  # area for smoothing options
+        for j in range(6):
+            smooth_areas.append(st.empty())
+        partition.append(smooth_areas) 
+        
+        user_interaction_areas.append(partition)
+    
+    
     partition = []
     
     partition.append(st.empty())  # area for derivative graph
@@ -396,73 +427,125 @@ for i in range(get_graph_session_info().num_derivatives):
     for j in range(6):
         smooth_areas.append(st.empty())
     partition.append(smooth_areas) 
-    
-    user_interaction_areas.append(partition)
-
-
-partition = []
-
-partition.append(st.empty())  # area for derivative graph
-
-partition.append(st.empty())  # area for removing derivative
-
-smooth_areas = []  # area for smoothing options
-for j in range(6):
-    smooth_areas.append(st.empty())
-partition.append(smooth_areas) 
-new_der_btn = st.empty()
-if new_der_btn.button("New Derivative"):
-    for dataset in plots_data:
-        for data_entry in dataset:
-            data_entry.new_derivative()
-            data_entry.update_plot_data(data_entry.num_plots)
-    get_graph_session_info().increment_derivative_count()
-    user_interaction_areas.append(partition)
-    new_der_btn.empty()
-    
-
-# configuration for derivative plots -- this also includes option to remove plot
-remove_next_derivative = False
-for i, partition in enumerate(user_interaction_areas):
-    if partition[1].button("Remove Derivative", key="7" + str(i + 1)) or remove_next_derivative:
+    new_der_btn = st.empty()
+    if new_der_btn.button("New Derivative"):
         for dataset in plots_data:
             for data_entry in dataset:
-                data_entry.remove_derivative_dataset(i + 1)
-        get_graph_session_info().set_derivative_count(data_entry.num_plots - 1)
-
-        remove_next_derivative = True
-        partition[1].empty()
-        continue  # move to next derivative number if derivative is removed
+                data_entry.new_derivative()
+                data_entry.update_plot_data(data_entry.num_plots)
+        get_graph_session_info().increment_derivative_count()
+        user_interaction_areas.append(partition)
+        new_der_btn.empty()
+        
     
+    # configuration for derivative plots -- this also includes option to remove plot
+    remove_next_derivative = False
+    for i, partition in enumerate(user_interaction_areas):
+        if partition[1].button("Remove Derivative", key="7" + str(i + 1)) or remove_next_derivative:
+            for dataset in plots_data:
+                for data_entry in dataset:
+                    data_entry.remove_derivative_dataset(i + 1)
+            get_graph_session_info().set_derivative_count(data_entry.num_plots - 1)
     
-    data = get_configuration(i + 1, partition[2], get_smoothing_options_session_info().get_settings(i + 1))
-    get_smoothing_options_session_info().set_settings(i + 1, data)
+            remove_next_derivative = True
+            partition[1].empty()
+            continue  # move to next derivative number if derivative is removed
+        
+        
+        data = get_configuration(i + 1, partition[2], get_smoothing_options_session_info().get_settings(i + 1))
+        get_smoothing_options_session_info().set_settings(i + 1, data)
+        
+        for dataset in plots_data:
+            for data_entry in dataset:
+                data_entry.update_configuration(i + 1, data)
+                data_entry.update_plot_data(i + 1)
     
-    for dataset in plots_data:
-        for data_entry in dataset:
-            data_entry.update_configuration(i + 1, data)
-            data_entry.update_plot_data(i + 1)
+    # fill in graphs with configured areas
+    formatted_data = {}
+    
+    # format data so it can be used by multi axis plots function
+    for axis, axis_data in enumerate(plots_data):
+        for i, dataset in enumerate(axis_data):
+            for derivative_number, data in enumerate(dataset.format_data(y_axis_titles[axis][i], x_axis_title)):
+                if not formatted_data.get(derivative_number):
+                    formatted_data.update({derivative_number:{axis+1:[data]}})
+                else:
+                    try:
+                        formatted_data.get(derivative_number).get(axis + 1).append(data)
+                    except AttributeError:
+                        formatted_data.get(derivative_number).update({axis + 1:[data]})
+    
+    plot_objects = plot_handler.create_multi_axis_plots(formatted_data, add_axis_title_to_legend)            
+    for a in user_interaction_areas:
+        areas.append(a[0])
+    # print(len(areas), len(plot_objects), plots_data[0][0].num_plots, get_graph_session_info().num_derivatives)
+       
+    for area, plt in zip(areas, plot_objects):
+        area.plotly_chart(plt, use_container_width=True)
+        
+        
+elif mode == "Parsed Data - Time Series":
+    data_type = st.sidebar.selectbox("Data Table Entry", sorted(data_options.keys()))
+    plotted_areas = get_regions(world_node)
+    
+    columns = {}
+    for node in plotted_areas:
+        data = getattr(node, data_options.get(data_type))
+        if callable(data):
+            data = data()
+            
+        if data:
+            columns.update({node.node_name:data})
+        else:
+            st.warning("No " + data_type + " data was found for " + node.node_name)
 
-# fill in graphs with configured areas
-formatted_data = {}
+    df = pd.DataFrame(columns, index=dates)
+    st.dataframe(df)
 
-# format data so it can be used by multi axis plots function
-for axis, axis_data in enumerate(plots_data):
-    for i, dataset in enumerate(axis_data):
-        for derivative_number, data in enumerate(dataset.format_data(y_axis_titles[axis][i], x_axis_title)):
-            if not formatted_data.get(derivative_number):
-                formatted_data.update({derivative_number:{axis+1:[data]}})
+    
+elif mode == "Parsed Data - Daily Reports":
+    plotted_areas = get_regions(world_node)
+    
+    for node in plotted_areas:
+        columns = {}
+        for data_name, data_attribute in data_options.items():
+            data = getattr(node, data_attribute)
+            if callable(data):
+                data = data()
+            if data:
+                columns.update({data_name:data})
             else:
-                try:
-                    formatted_data.get(derivative_number).get(axis + 1).append(data)
-                except AttributeError:
-                    formatted_data.get(derivative_number).update({axis + 1:[data]})
+                st.warning("No " + data_name + " data was found for " + node.node_name)
+        
+        df = pd.DataFrame(columns, index=dates)
+        st.write("Dataframe for ", get_label(node))
+        st.dataframe(df)
 
-plot_objects = plot_handler.create_multi_axis_plots(formatted_data, add_axis_title_to_legend)            
-for a in user_interaction_areas:
-    areas.append(a[0])
-# print(len(areas), len(plot_objects), plots_data[0][0].num_plots, get_graph_session_info().num_derivatives)
-   
-for area, plt in zip(areas, plot_objects):
-    area.plotly_chart(plt, use_container_width=True)
+elif mode == "File Data":
+    # make directories for data to be held if they do not already exist
+    if not os.path.isdir(os.path.join(os.path.dirname("."), "data")):
+        os.mkdir(os.path.join(os.path.dirname("."), "data"))
     
+    # check if most recent data is downloaded
+    all_subdirs = []
+    for d in os.listdir('./data'):
+        bd = os.path.join('./data', d)
+        if os.path.isdir(bd):
+            all_subdirs.append(bd)
+            
+    if all_subdirs:
+        latest_subdir = max(all_subdirs, key=os.path.getmtime)
+    else:
+        latest_subdir = "."
+            
+    if str(datetime.date.today()) not in latest_subdir:
+        if st.button("Download Data"):  # download data if it is not present
+            with st.spinner("Retreiving Latest Data, Please Wait..."):
+                data_grabber.retrieve_data()
+                
+    else:
+        st.success("Current Data is already saved to disk")
+        if st.button("Download Data Again"):  # download data if it is not present
+            with st.spinner("Retreiving Latest Data, Please Wait..."):
+                data_grabber.retrieve_data()
+            
